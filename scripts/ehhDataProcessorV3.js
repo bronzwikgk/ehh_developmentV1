84ab51158a201a18163e78aeaa3e4a7b835cd5eb
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2020-11-16 22:14:51 +0530

    u

--- scripts/ehhDataProcessorV3.js
+++ scripts/ehhDataProcessorV3.js
@@ -31,24 +31,24 @@ function createEntity(input, output, outputType, model2Process) {
 function iterateArrayEntity(input, iterateArrayResponse, outputType) {
    // console.log("iterateArrayResponse",iterateArrayResponse);
     if (!iterateArrayResponse) { var iterateArrayResponse = []; }
-       console.log("input", input, typeof input, input.length, isObjectArray_(input), isArray(input));
+      // console.log("input", input, typeof input, input.length, isObjectArray_(input), isArray(input));
        for (i = 0; i <= input.length; i++) {
         if (typeof input === 'object') {
             var key = {};
             if (input[i] != 'undefined' && typeof (input[i]) != "undefined") {
              //  console.log(input[i],iterateArrayResponse);
                 key[input[i].name] = input[i].value;
-               // console.log("temp", temp);
+               console.log("Key", key);
                 iterateArrayResponse = setEntity(input, iterateArrayResponse, key);
             }
         }
     }
-    console.log("iterateArrayResponse",iterateArrayResponse);
+   console.log("iterateArrayResponse",iterateArrayResponse);
     return iterateArrayResponse;
 }
 
 function setEntity(input, output,key ,outputType) {
-    console.log(output, typeof output,);
+    //console.log(output, typeof output,);
     if (getEntityType(output) === 'Array') { 
         output.push(key);
     }
@@ -65,7 +65,7 @@ function iterateObjectEntity(input, iterateObjResponse, outputType) {
     if (!iterateObjResponse) { var iterateObjResponse = {}; }
   //  console.log("input", input, typeof input, input.length, isObjectArray_(input), isArray(input));
     for (key in input) { 
-     //  console.log("foundKey", key, input[key], input[key].valueOf());
+       //console.log("foundKey", key, input[key], input[key].valueOf());
         if (key && input[key] != undefined && input[key] != 'function' && typeof (input[key].value) != "undefined") { 
        
             console.log("foundKey", key, input[key], input[key].valueOf());
