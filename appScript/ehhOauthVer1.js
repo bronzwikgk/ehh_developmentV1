812521c635db1234a281d4a37438958649381a96
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2020-11-20 12:01:24 +0530

    u

--- appScript/ehhOauthVer1.js
+++ appScript/ehhOauthVer1.js
@@ -1,15 +1,36 @@
+let deployedURL ="https://script.google.com/macros/s/AKfycbzs1uHX-vtR_Vj2uwNoBvOt5kHAO0m8t9eFXOujtgof9pJW0qqA/exec"
 let serverSheetID = "1cAbSk2mLzwuLwLgKxX_-Ve8-8UrUgSCWG7qj_OQ7MVM";
 let serverApiUrl = " sheet api";
 
+
+function doGet(e){
+
+  return response = ehhServer.doGet(e);
+}
+
+
+function doPost(e){
+  var requestType = (e.parameter.TypeOfRequest);
+  if (requestType == "updateUserInformation") {
+    return updateUserInformation(e);
+  }
+  if (requestType == "signUpUser") {
+    return signUpUser(e);
+  }
+  if (requestType == "loginUser"){
+    return loginUser(e);
+  }
+}
+
 class ehhServer {
   static doGet(e) {
-    Logger.log(e);
     var op = e.parameter.action;
+    Logger.log(e,op);
     var ss = SpreadsheetApp.openByUrl("https://docs.google.com/spreadsheets/d/1UUBGKGvm4b9LOhqbdh0UDG6vYz9Phr1DqGyg8DvCiUc/edit?usp=sharing");
     var sheet = ss.getSheetByName("Sheet1");
     if (op == "insert")
+      
       return insert_value(e, sheet);
-
     //Make sure you are sending proper parameters
     if (op == "read")
       return read_value(e, ss);
@@ -23,7 +44,6 @@ class ehhServer {
   }
   static doPost() {
     
-
 
   }
 
