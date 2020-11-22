a29908f12d05a67a40248188f62947375d5bd008
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2020-11-22 22:39:44 +0530

    u

--- scripts/ehhjson2Array.js
+++ scripts/ehhjson2Array.js
@@ -12,9 +12,10 @@ function processRequest() {
 
 function fetchRequest(requestURL) { 
      var requestResponse = fetch(requestURL)
-    .then(response => response.json())
+    .then(response => { return response.json()})
     .then(function (data){
      console.log("data Recived",data)
+     getPayload(data);
       return data;
     });
    console.log("requestResponse", requestResponse);
@@ -25,7 +26,7 @@ function fetchRequest(requestURL) {
 
 
 function getPayload(payload,payloadOutput) {
-   // console.log(payload);
+    console.log(payload,payload);
     var payloadOutput = json2Array(payload);
     return payloadOutput;
 }
@@ -37,28 +38,37 @@ function json2Array(input, json2ArrayOutput) {
     if (typeof input === 'object' && !input.length) {
       //  console.log("Found Object");
         json2ArrayOutput = iterateObj(input, json2ArrayOutput);
-        
-      
-
     }
     console.log("json2ArrayOutput",json2ArrayOutput);
     return json2ArrayOutput;
 }
 
 
-function iterateObj(input,iterateObjOutput,options) {
+function iterateObj(input,iterateObjOutput,parent,options) {
     if (!input) return;
-    if (!iterateObjOutput) { var iterateObjOutput = {}; };
+    if (!iterateObjOutput) { var iterateObjOutput = []; };
     for (var key in input) {
 
         if ({}.hasOwnProperty.call(input, key)) {
 
             if (typeof input[key] === 'object' && !input[key].length) { 
-
                 console.log("childFound", key, input[key]);
                 recurseResponse = [];
-                recurseResponse = iterateObj(input[key], recurseResponse);
-
+                recurseResponse.concat(key);
+                recurseResponse.concat(parent);
+                iterateObj(input[key], recurseResponse,key);
+                // iterateObjOutput = setData(input, iterateObjOutput, key);
+                iterateObjOutput.push(recurseResponse);
+                iterateObjOutput = setData(recurseResponse, iterateObjOutput, key);
+            } else if (input[key].length) { 
+                console.log("arrayFound", key, input[key]);
+                // recurseResponse = [];
+                // recurseResponse.concat(key);
+                // recurseResponse.concat(parent);
+                // iterateObj(input[key], recurseResponse,key);
+                // // iterateObjOutput = setData(input, iterateObjOutput, key);
+                // iterateObjOutput.push(recurseResponse);
+                // iterateObjOutput = setData(recurseResponse, iterateObjOutput, key);
             }
            // console.log(key,input[key]);
             iterateObjOutput = setData(input, iterateObjOutput, key);
@@ -77,7 +87,12 @@ function setData(input, output, key) {
   //  console.log(key, getEntityType(output), output.length);
     if (getEntityType(output) === 'Array') { 
       //  console.log(key);
+      if(!input){
         output.push(key);
+      } else{
+        output.push(input);
+      }
+        
     }
   // console.log("output from Set",output);
     return output;
