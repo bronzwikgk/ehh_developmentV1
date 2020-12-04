44ba69e68ffefc3e466036a823695c90ad667d2b
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2020-12-04 17:24:04 +0530

    u

--- scripts/arr2ObjV3.js
+++ scripts/arr2ObjV3.js
@@ -19,6 +19,7 @@ function arr2Obj(inputTable, output, currentObj, currentRow) {
     console.log(output)
     return output;
 }
+
 function getChildren(input, currentObj, currentRow,inputTable) {
 
     if (!output) { var output = {}; }
@@ -26,7 +27,13 @@ function getChildren(input, currentObj, currentRow,inputTable) {
     maxDepth = Math.max(...splitArray(input, 2));
     //console.log("input", inputTable)
     childrenObj = {};
-    children = input.filter((row, value) => { if (row[2] === currentRow[3] && row[1] === currentRow[1] + 1) return row; });
+    children = input.filter((row, value) => {
+        if (row[2] === currentRow[3] && row[1] === currentRow[1] + 1) {
+            console.log("delteing", inputTable[i], "currentObect", currentObj)
+
+            inputTable.splice(row[1], 1);
+            return row;   
+    } });
 
   //  console.log("children",children) 
     for (i = 1; i < children.length; i++) {
@@ -37,21 +44,19 @@ function getChildren(input, currentObj, currentRow,inputTable) {
             currentObj[currentRow[3]] = currentObj;
             }
         }
-   
-  //  console.log(currentObj)
-  //  return currentObj;
 }
+
 function row2object(inputRow, outputObj, currentObj, currentRow,inputTable) {
     if (!currentObj) { var currentObj = {}; }
     attributes = updateAttributesNvalues(inputTable, currentObj, currentRow, inputTable);
-    console.log("delteing", inputTable[i], "currentObect", currentObj)
-    inputTable.splice(inputTable[i], 1);
+   
   //  getChildren(inputTable, currentObj, currentRow);
    // console.log( "currentObect", currentObj)
     //if (children.length>0) { console.log("children", children) }
      set(attributes, currentObj, inputRow[3]);
     return currentObj;
 }
+
 function set(input, output, key) {
     switch (output?.constructor) {
         case Object:
@@ -62,6 +67,7 @@ function set(input, output, key) {
             return "Output UnIdentified"
     }
 }
+
 function updateAttributesNvalues(inputTable, currentObj, currentRow) {
     header = inputTable[0];
     rowAttributes = currentRow.slice(2);
