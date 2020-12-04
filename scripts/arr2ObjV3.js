6203f6633a51da95869d42895118b1dbc4d8b540
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2020-12-04 16:03:34 +0530

    u

--- scripts/arr2ObjV3.js
+++ scripts/arr2ObjV3.js
@@ -10,31 +10,56 @@ function arr2Obj(inputTable, output, currentObj, currentRow) {
         for (i = 1; i < inputTable.length; i++) { 
             if (inputTable[i][1] === d) { 
                 currentRow = inputTable[i];
-                currentObj = row2object(currentRow, currentObj, currentObj, currentRow,inputTable);
-                console.log("CurrentObj",currentObj);
+                currentObj = row2object(currentRow, currentObj, currentObj, currentRow, inputTable);
+                console.log("delteing", inputTable[i], "currentObect", currentObj)
+                inputTable.splice(inputTable[i], 1);
                 output[currentRow[3]] = currentObj;
             }
         }
-        
     }
     console.log(output)
     return output;
 }
+function getChildren(input, currentObj, currentRow,inputTable) {
 
+    if (!output) { var output = {}; }
+    //console.log(input)
+    maxDepth = Math.max(...splitArray(input, 2));
+    //console.log("input", inputTable)
+    childrenObj = {};
+    children = input.filter((row, value) => { if (row[2] === currentRow[3] && row[1] === currentRow[1] + 1) return row; });
 
+    console.log("chidlren",children) 
+    for (i = 1; i < children.length; i++) {
+        if (children[i][1] === d) {
+            currentRow = children[i];
+                currentObj = row2object(currentRow, currentObj, currentObj, currentRow, inputTable);
+            console.log("delteing", input[i], "currentObje", currentObj);
+            input.splice(inputTable[i], 1);
 
-
+              //  console.log("children", children);
+                  childrenObj = getChildren(children, currentObj, currentObj, currentRow, inputTable);
+                console.log("childrenObj", childrenObj);
+                //  console.log(inputTable)
+                //   break;
+                // console.log("CurrentObj",currentObj);
+            currentObj[currentRow[3]] = currentObj;
+            }
+        }
+   
+    console.log(currentObj)
+    return currentObj;
+}
 function row2object(inputRow, outputObj, currentObj, currentRow,inputTable) {
     if (!currentObj) { var currentObj = {}; }
     attributes = updateAttributesNvalues(inputTable, currentObj, currentRow, inputTable);
- 
+    
+    getChildren(inputTable, currentObj, currentRow);
+    
+    if (children.length>0) { console.log("children", children) }
      set(attributes, currentObj, inputRow[3]);
-   
     return currentObj;
-
-
- }
-
+}
 function set(input, output, key) {
     switch (output?.constructor) {
         case Object:
@@ -45,7 +70,6 @@ function set(input, output, key) {
             return "Output UnIdentified"
     }
 }
-
 function updateAttributesNvalues(inputTable, currentObj, currentRow) {
     header = inputTable[0];
     rowAttributes = currentRow.slice(2);
@@ -62,7 +86,6 @@ function updateAttributesNvalues(inputTable, currentObj, currentRow) {
         return attrs;
     }
 }
-
 //This function takes an array as input and extract a column as a return array
 function splitArray(input, column) {
     var output = [];
