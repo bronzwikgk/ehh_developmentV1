6398185e2728555a98f934a4a3eeb02d615846e2
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2020-12-07 09:32:31 +0530

    u

--- scripts/ehhDataHelpersV3.js
+++ scripts/ehhDataHelpersV3.js
@@ -182,3 +182,31 @@ class dataHelpers {
         return true;
     }
 }
+
+
+
+
+
+
+//javascript create JSON object from two dimensional Array
+function arrayToJSONObject(arr) {
+    //header
+    var keys = arr[0];
+    output = {}
+    //vacate keys from main array
+    var newArr = arr.slice(1, arr.length);
+
+    var formatted = [],
+        data = newArr,
+        cols = keys,
+        l = cols.length;
+    for (var i = 0; i < data.length; i++) {
+        var d = data[i],
+            o = {};
+        for (var j = 0; j < l; j++)
+            o[cols[j]] = d[j];
+        output[d] = o;
+
+    }
+    return output;
+}
