f74b3d8b7c20136ed54e2cc7304db9cc4156cb61
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2020-12-20 11:03:35 +0530

    u

---
+++ Old/processUrlinTest.js
@@ -0,0 +1,39 @@
+// server Sheet : https://docs.google.com/spreadsheets/d/1mBZJ01Ddhl9fByyJ5K-JEsruO7XV51i7ctdnRJ6-hGk/edit#gid=1449573075 
+const resourceLocation = "https://docs.google.com/spreadsheets/d/1mBZJ01Ddhl9fByyJ5K-JEsruO7XV51i7ctdnRJ6-hGk/edit#gid=1449573075";
+const url = "https://script.google.com/macros/s/AKfycbxeONL9wDhS1GOnHJapV-67BMKFQk-k9WMA5m4C77mROTCipMQ/exec";
+
+function makeDoGetRequest() {
+    console.log(" In DG function:");
+    fetch(url)
+        .then(res => res.json())
+        .then(res => {
+            console.log(res);
+            document.getElementById("getResponse").textContent = res[0].status;
+        });
+}
+
+function makeDoPostRequest() {
+    console.log(" In AddRow function:");
+    fetch(url, {
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'no-cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        //credentials: 'same-origin', // include, *same-origin, omit
+        // headers: {
+        //     'Content-Type': 'application/json'
+        //         // 'Content-Type': 'application/x-www-form-urlencoded',
+        // },
+        redirect: 'follow', // manual, *follow, error
+        //   referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+        body: JSON.stringify({
+                name: "Testing doPost"
+            }) // body data type must match "Content-Type" header
+    })
+    .then(res => res.json())
+    .then(res => {
+        document.getElementById("postResponse").textContent = res[0].status;
+    });
+}
+
+document.getElementById("btn").addEventListener("click", makeDoGetRequest);
+document.getElementById("btn2").addEventListener("click", makeDoPostRequest);
--- scripts/clientNodeFetchV1.js
+++ scripts/clientNodeFetchV1.js
@@ -5,8 +5,6 @@ var data = {
         phoneNumber: "phoneNumber",
         city: "city",
     };
-
-
 var resourceUrl = "https://docs.google.com/spreadsheets/d/1mBZJ01Ddhl9fByyJ5K-JEsruO7XV51i7ctdnRJ6-hGk/edit#gid=1449573075";
 const serviceUrl =  "https://script.google.com/macros/s/AKfycbxeONL9wDhS1GOnHJapV-67BMKFQk-k9WMA5m4C77mROTCipMQ/exec";
 request = {
@@ -23,43 +21,18 @@ request = {
 //    // referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
    body: JSON.stringify(data) // body data type must match "Content-Type" header
 };
-
 postRequest = {
-    // Default options are marked with *
-    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-     mode: 'no-cors', // no-cors, *cors, same-origin
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-//    // credentials: 'same-origin', // include, *same-origin, omit
-   // headers: {
-   //     'Content-Type': 'application/json'
-   //     // 'Content-Type': 'application/x-www-form-urlencoded',
-   // },
-    redirect: 'follow', // manual, *follow, error
-//    // referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-   body: JSON.stringify(data) // body data type must match "Content-Type" header
-}
-
-
-//while making a get Req with Query parameters url has to build from the process Url
-// when no parameter it return a seems, you have lost you way message.
-//when making a post reqst a normal post works.
-function processGet(e) {
-    e.preventDefault();
-    console.log(e.target.id);
-    //  params1 =? TypeOfRequest = signUpUser;
-    let url = serviceUrl;
-    var encodedParam = buildEncodedUri(data);
-    //  console.log(url)
-    //  var decodedParam = unbuildEndodedUri(encodedParam);
-    var url2 = url + "?" + encodedParam;
-    console.log(url2)
-    fetchHttpRequest(url2);  
+    method: 'POST',
+    mode: 'cors',
+    cache: 'no-cache',
+    redirect: 'follow',
+    body: JSON.stringify(data)
 }
 
 function fetchHttpRequest(url, request) {
     fetch(url, request)
         .then(response => {
-          //  if (!response.ok) { throw new Error("Could not reach website."); }
+            //  if (!response.ok) { throw new Error("Could not reach website."); }
             return response.json();
         })
         .then(json => console.log(json))
@@ -67,63 +40,28 @@ function fetchHttpRequest(url, request) {
 
 }
 
-//while making a get Req with Query parameters url has to build from the process Url. Note the parameter are directly the body
-// when no parameter in the get Req "it return a seems, you have lost you way" message is responded back.
+
+//To make a getReq to a appScript server the queryParameter have to be added to the url using buildEncodedURI.
 //when making a post reqst a normal post works.
-function processPost(e) {
-    
+function processGet(e) {
     e.preventDefault();
     console.log(e.target.id);
     //  params1 =? TypeOfRequest = signUpUser;
     let url = serviceUrl;
-    //  let url1 = new URL(reqUrl);
-  //  params1 = { status: "notCool!" }
-    //  console.log(url)
-    //  var decodedParam = unbuildEndodedUri(encodedParam);
-    request = {
-        // Default options are marked with *
-        method: 'POST', // *GET, POST, PUT, DELETE, etc.
-        mode: 'cors', // no-cors, *cors, same-origin
-        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-        //    // credentials: 'same-origin', // include, *same-origin, omit
-        // headers: {
-        //     'Content-Type': 'application/json'
-        //     // 'Content-Type': 'application/x-www-form-urlencoded',
-        // },
-        redirect: 'follow', // manual, *follow, error
-        //    // referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: JSON.stringify(data) // body data type must match "Content-Type" header
-    }
-   postParam =  {
-        method: 'POST',
-            mode: 'cors',
-                cache: 'no-cache',
-                    redirect: 'follow',
-                        body: JSON.stringify(data)
-    }
-    
-    fetch(url, postParam )
-        .then(res => res.text())
-        .then(res => {
-            console.log(res);
-            // customerForm.reset();
-            // buttonText.textContent = "Send";
-            // buttonSpinner.classList.add("d-none");
-            // submitButton.disabled = false;
-        })
-        .catch(err => console.error(err)); 
-  //  console.log(url2)
-   // fetchUrl2(url, request);  
-    // fetch(url,request)
-    //     .then(response => {
-    //       //  if (!response.ok) { throw new Error("Could not reach website."); }
-    //         return response.text();
-    //     })
-    //     .then(data => console.log(data))
-    //    
+    var encodedParam = buildEncodedUri(data);
+    var url2 = url + "?" + encodedParam;
+    console.log(url2)
+    fetchHttpRequest(url2);  
 }
 
-///USed to encode Params 
+function processPost(e) {
+    e.preventDefault();
+    console.log(e.target.id);
+    let url = serviceUrl;
+    fetchHttpRequest(url,postRequest);  
+}
+
+///it takes a inputobject and build's it into a encodedURI
 function buildEncodedUri(request) {
     const response = [];
     for (let d in request)
@@ -151,5 +89,5 @@ function unbuildEndodedUri(request) {
 
 
 
-document.getElementById("btn").addEventListener("click", processGet);
-document.getElementById("btn2").addEventListener("click", processPost);
+document.getElementById("get").addEventListener("click", processGet);
+document.getElementById("post").addEventListener("click", processPost);
