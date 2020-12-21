933d55b6d8ac351e4770e198ac3d1c0fc2567ba9
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2020-12-22 02:01:22 +0530

    u

--- scripts/processSchemaV3.js
+++ scripts/processSchemaV3.js
@@ -1,5 +1,7 @@
 
-var formElements = ['button', 'datalist', "div", 'fieldset', "form", "input", "label", 'legend', 'li', 'meter', 'optgroup', "option", 'output', 'progress', "select", "span", "textarea", "ul"];
+var formElements = ["button", 'datalist', "div", 'fieldset', "form", "input", "label", 'legend', 'li', 'meter', 'optgroup', "option", 'output', 'progress', "select", "span", "textarea", "ul"];
+var attributes = ["type","title","class","id","name","value","required"]
+
 
 
 class processSchema { 
@@ -25,16 +27,23 @@ class processSchema {
             if (formElements.indexOf(input) < 0) { //check if the input is a formElement by crosschecking in the define array.
                // console.log("createRequest for ", input, output, value, formElements.indexOf(input))
                var nwEle = document.createElement("div");
-                nwEle.className = input;
+
+               // nwEle.className = input;
               // console.log("divElement", nwEle);
             } else {
                var nwEle = document.createElement(input);
                // console.log("formElement", nwEle);
             }
+
+            if(attributes.indexOf(input)>0){
+            output.setAttribute(key,value);
+                //console.log("attrbutes found",key,value)
+
+            }
             if(typeof value === 'string') {
                 var content = document.createTextNode(value);
                 nwEle.appendChild(content);
-                nwEle.setAttribute("value", value);
+              //  nwEle.setAttribute("value", value);
                 //  console.log("setting Attributes", key, input, "in", output)
                // output.setAttribute(input, value);
             }
@@ -81,12 +90,13 @@ class processSchema {
             } else if (getEntityType(input[key]) === 'Array') {  
                 processSchema.schema2(input[key], currentNode,key,input[key]);
                 //var currentNode = processSchema.create(key, output,key,input[key]);
-                  //  processSchema.appendChild(currentNode, output);
+                 processSchema.appendChild(currentNode, output);
             }else if (getEntityType(input[key]) === 'String' || getEntityType(input[key]) === 'Function' || getEntityType(input[key]) === 'Boolean') {  
                 
-                var currentNode = processSchema.create(key, output,key,input[key]);
+                    var currentNode = processSchema.create(key, output,key,input[key]);
                     processSchema.appendChild(currentNode, output);
             } else {
+                console.log("strays")
             }
 
         }
@@ -100,8 +110,9 @@ class processSchema {
             } else if (getEntityType(input[i]) === 'Array') {
                 console.log("found Object in array", input[i])
             } else if (getEntityType(input[i]) === 'String' || getEntityType(input[i]) === 'Function' || getEntityType(input[i]) === 'Boolean') {
+                
                 var currentNode = processSchema.create(key, output, input[i], input[i]);
-                processSchema.appendChild(currentNode, output);
+                    processSchema.appendChild(currentNode, output);
             } else {
             }
             //            console.log(input[i], getEntityType(input[i]));
@@ -117,10 +128,6 @@ class processSchema {
 
     }
 }
-
-
-
-
 
 function getEntityType(entity) {
     return Object.getPrototypeOf(entity).constructor.name;//entity.__proto__.constructor.name
@@ -142,8 +149,28 @@ function processTest(e) {
     // outputJson = arr2(outputArray,{} ,depth);
     // console.log(outputJson);
     //  document.getElementById("output").innerText = JSON.stringify(outputArray);
-  /  document.getElementById("output").appendChild(outputE);
+    document.getElementById("output").appendChild(outputE);
 }
 
 
 document.getElementById("get").addEventListener("click", processTest);
+
+
+// var a = {
+//     div:{
+//         'class': "preference",
+//         "label": "do you like cheese",
+//         'type' : "checkBox"
+//     }
+
+// }
+{/* <div class="preference">
+    <label for="cheese">Do you like cheese?</label>
+    <input type="checkbox" name="cheese" id="cheese">
+</div>
+
+<div class="preference">
+    <label for="peas">Do you like peas?</label>
+    <input type="checkbox" name="peas" id="peas">
+</div> */}
+
