d182501d886c093ba8343ba63d31349e4fa8b0a5
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2020-12-21 11:31:56 +0530

    u

--- scripts/arr2Objv7.js
+++ scripts/arr2Objv7.js
@@ -4,6 +4,9 @@
 // Find the parent element of the current element
 // In the parent element’s object, add a reference to the child
 // If there is no parent for an element, we know that will be our tree’s “root” element
+//https://github.com/alferov/array-to-tree/blob/master/index.js
+//https://stackoverflow.com/questions/18017869/build-tree-array-from-flat-array-in-javascript
+//https://github.com/iconifyit/30-Days-of-Algorithms/blob/master/src/05-array-to-tree.js
 
 function child(entity,parent) {
     
@@ -11,6 +14,7 @@ function child(entity,parent) {
         console.log(entity,"is child of ",parent);
     } 
 }
+
 
 function buildTree(input) {
 
@@ -44,6 +48,7 @@ function updateAttributesNvalues(input,currentRow) {
     }
    
 }
+
 function arr2Obj(input) {
 
     var inputTable = [...input];
@@ -151,4 +156,47 @@ function listToTree(data, options) {
     };
 
     return tree;
+}
+
+
+//**
+//* This method is used to convert an array with parent - child relationship into an array with tree structure
+//* Receive an array with parent - child relationship as a parameter
+//* Returns an array of tree structures
+//** */
+function translateDataToTree(data) {
+    //Data without parent node
+    let parents = data.filter(value => value.parentId == 'undefined' || value.parentId == null)
+
+    //Data with parent node
+    let childrens = data.filter(value => value.parentId !== 'undefined' && value.parentId != null)
+
+    //Define the concrete implementation of transformation method
+    let translator = (parents, childrens) => {
+        //Traverse parent node data
+        parents.forEach((parent) => {
+            //Traversal of child node data
+            childrens.forEach((current, index) => {
+                //At this time, find a child node corresponding to the parent node
+                if (current.parentId === parent.id) {
+                    //Deep replication of sub node data is only supported here. For children's boots that don't know about deep replication, you can first learn about deep replication
+                    let temp = JSON.parse(JSON.stringify(childrens))
+                    //Let the current child node be removed from temp, which is the new data of child nodes. This is to make the number of iterations of child nodes less during recursion. The more layers of parent-child relationship, the more favorable
+                    temp.splice(index, 1)
+                    //Let the current child node be the only parent node to recursively find its corresponding child node
+                    translator([current], temp)
+                    //Put the found child node in the children attribute of the parent node
+                    typeof parent.childrens !== 'undefined' ? parent.childrens.push(current) : parent.childrens = [current]
+                }
+            }
+            )
+        }
+        )
+    }
+
+    //Call transformation method
+    translator(parents, childrens)
+
+    //Return the final result
+    return parents
 }
