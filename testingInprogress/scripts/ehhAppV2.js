d277eb5b9905dcfae18e7c97c7e27a9851dc4eea
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2020-12-23 13:01:09 +0530

    u

---
+++ testingInprogress/scripts/ehhAppV2.js
@@ -0,0 +1,18 @@
+
+class app {
+
+    static init() {
+        console.log("arguments.callee.name")
+    }
+    static gotMessage(message, sender, sendResponse) {
+        
+    }
+
+    static sendMessage(recipient, message) {
+        
+    }
+
+
+}
+
+app.init();
