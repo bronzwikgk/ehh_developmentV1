8d3ba4cd3d7ada8df8f12b1b1f40cc2848d35b6f
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2020-12-24 18:44:35 +0530

    u

--- scripts/actionEditor.js
+++ scripts/actionEditor.js
@@ -1,28 +1,37 @@
 
 var actionEditor = document.getElementById("ehhActionEditor");
 
-actionEditor.addEventListener('keydown', refresh);
+//actionEditor.addEventListener('keydown', refresh);
 actionEditor.addEventListener('keyup', refresh);
 
-var jsonSyantax = ["{","}"]
+var jsonSyantax = ["{","["]
+
+
+
+
 
 function refresh(e) { 
     var actionEditor = document.getElementById("ehhActionEditor");
     var output = document.getElementById("output");
- //   console.log(e.key, e.code)
-  //  console.log(actionEditor.value)
     var buffer = actionEditor.value;
     processBuffer(e,buffer);
  //   console.log(buffer);
     output.innerHTML = buffer;
+    buffer = "";
 }
 
-function processBuffer(event, input) {
+function processBuffer(event, input,buffer) {
    // console.log(event.key, event.which,input)
     //detect Short Cut....Look Up Short Dic..Execute Command
     //Detect Sytax 
-
-    console.log(validate.isOneof(event.key,jsonSyantax))
+    if (validate.isOneof(event.key, jsonSyantax)) { 
+       
+        actionEditor.value = actionEditor.value.toString().concat("}")         
+      //  JSON.stringify(buffer).concat("}")
+        console.log("Found match", event.key, event.target.selectionStart, actionEditor.value  )
+        
+    }
+    
 }
 function executeRule(validation, callbackIfTrue, CallBackIfFalse) { 
     
@@ -37,7 +46,7 @@ class validate {
         return Object.keys(obj).length === 0 ? true : false;
     }
     static isOneof(a, b, key, value) {
-        console.log(a,b)
+      //  console.log(a,b)
       return b.indexOf(a) > -1 ? true : false;
     }
    
@@ -49,26 +58,47 @@ class validate {
     }
 
 }
-// console.log(output)
-// function compile() {
-//     var html = document.getElementById("html");
-//     var css = document.getElementById("css");
-//     var js = document.getElementById("js");
-//     var code = document.getElementById("code").contentWindow.document;
 
-//     document.body.onkeyup = function () {
-//         code.open();
-//         code.writeln(
-//             html.value +
-//             "<style>" +
-//             css.value +
-//             "</style>" +
-//             "<script>" +
-//             js.value +
-//             "</script>"
-//         );
-//         code.close();
-//     };
-// }
 
-// compile();
+async function start() {
+    /**
+     * Setup a new engine
+     */
+    const engine = new Engine()
+
+    /**
+     * Create a rule
+     */
+    engine.addRule({
+        // define the 'conditions' for when "hello world" should display
+        conditions: {
+            all: [{
+                fact: 'displayMessage',
+                operator: 'equal',
+                value: true
+            }]
+        },
+        // define the 'event' that will fire when the condition evaluates truthy
+        event: {
+            type: 'message',
+            params: {
+                data: 'hello-world!'
+            }
+        }
+    })
+
+    /**
+     * Define a 'displayMessage' as a constant value
+     * Fact values do NOT need to be known at engine runtime; see the
+     * 03-dynamic-facts.js example for how to pull in data asynchronously during runtime
+     */
+    const facts = { displayMessage: true }
+
+    // engine.run() evaluates the rule using the facts provided
+    const { events } = await engine.run(facts)
+
+    events.map(event => console.log(event.params.data.green))
+}
+
+// start()
+
