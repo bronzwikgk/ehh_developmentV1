a3084e65ac12d7404756e6870e060b4d70055bb3
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2020-12-26 17:49:38 +0530

    u

--- scripts/actionEditor.js
+++ scripts/actionEditor.js
@@ -79,7 +79,7 @@ function deserialize(params) {
 // this.accepts = [{ 
 //  mimeTypes: ['text/*'],
    // extensions: ['js', 'css', 'txt', 'html', 'xml', 'tsv', 'csv', 'rtf']
-    }];
+ //   }];
 //         this.textBuffer = { point, range },
 //         this.options = {
 //         autofocus: true,
