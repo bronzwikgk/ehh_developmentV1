be9432979d82ad8eb23688fb5093889170a5316b
bronzwikgk <67652347+bronzwikgk@users.noreply.github.com>
2020-12-27 22:27:56 +0530

    u

--- Old/scripts/ehhDataHelpersV3.js
+++ Old/scripts/ehhDataHelpersV3.js
@@ -1,14 +1,5 @@
 
-//This function validates a valid Url, Returns True or false
-function isValidUrl(string) {
-    try {
-        new URL(string);
-    } catch (_) {
-        return false;
-    }
 
-    return true;
-}
 
 
 /** Returns the name of a set property in an object, or else "unknown". */
@@ -89,14 +80,7 @@ function save(entity, keyTitle) {
 // console.log("With Valid JSON Text: "+IsValidJSONString(validjsontext));
 // console.log("With inValid JSON Text: "+IsValidJSONString(invalidjsontext));
 
-function IsValidJSONString(str) {
-    try {
-        JSON.parse(str);
-    } catch (e) {
-        return false;
-    }
-    return true;
-}
+
 
 function containsFiles(event) {
 
@@ -117,75 +101,9 @@ function isObject_(test) {
     return Object.prototype.toString.call(test) === '[object Object]';
 }
 
-/** 
- * Returns true if the given test value is an array containing at least one object; false otherwise.
- */
-function isObjectArray_(test) {
-    for (var i = 0; i < test.length; i++) {
-        if (isObject_(test[i])) {
-            return true;
-        }
-    }
-
-    return false;
-}
 
 
 
-const items = [1, false, "Devsage", 3.14]
-
-function Iterator(Array)
-{
-    this.Array = Array
-  this.index = 0
-}
-
-
-Iterator.prototype = {
-  hasNext: function()
-  {
-    return this.index < this.items.length
-  },
-  next: function()
-  {
-    return this.items[this.index++]
-  }
-}
-
-// const iter = new Iterator(items)
-
-// console.log(iter.hasNext())
-
-// while(iter.hasNext())
-//   console.log(iter.next())
-
-// console.log(iter.hasNext())
-
-
-
-
-class dataHelpers { 
-    //This function compares two array's
-    static compare2Array(arr1, arr2) {
-        arr1.sort(); arr2.sort();
-        if (arr1.length !== arr2.length) return false;
-        for (let i = 0; i < arr2.length; i++) {
-            if (arr[i] !== arr2[i]) return false;
-        }
-        return true;
-    }
-}
-
-//This function validates a valid Url, Returns True or false
-function isValidUrl(string) {
-    try {
-        new URL(string);
-    } catch (_) {
-        return false;
-    }
-
-    return true;
-}
 
 
 
